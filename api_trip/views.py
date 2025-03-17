242f82687e071227419903a1adfd7e7faeb81263
Boubacar Demba Mandiang <mandiangboubacardemba@gmail.com>
2025-03-17 17:36:03 +0000

    Setup trip api views

--- api_trip/views.py
+++ api_trip/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from rest_framework import viewsets, status
+from .models import Trip
+from .serializers import TripSerializer
 
-# Create your views here.
+class TripViewSet(viewsets.ModelViewSet):
+    queryset = Trip.objects.all()
+    serializer_class = TripSerializer
