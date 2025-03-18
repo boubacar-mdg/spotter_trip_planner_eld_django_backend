6f117942036d06ff4a83b99d7618ca86a4744a32
Boubacar Demba Mandiang <mandiangboubacardemba@gmail.com>
2025-03-18 21:26:25 +0000

    Deactivate cors middleware

--- spotter_trip_planner_eld_django_backend/settings.py
+++ spotter_trip_planner_eld_django_backend/settings.py
@@ -53,16 +53,15 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
+    #'corsheaders.middleware.CorsMiddleware',
 ]
 
-""" CORS_ALLOW_ALL_ORIGINS = True """
+CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = [
     "http://spotter-api.lavandesn.com",
     "https://spotter-api.lavandesn.com",
     "https://spotter.lavandesn.com",
     "http://spotter.lavandesn.com",
-    "spotter-api.lavandesn.com",
 ]
 
 
